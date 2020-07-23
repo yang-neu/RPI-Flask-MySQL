4c67cfabed5f38f42d72a0a539eb69849a73394c
Alex <alexandaryang@gmail.com>
2020-07-23 23:28:30 +0900

    Shift dhtWebServer from SQLite to MySQL

--- dhtWebServer/appDhtWebServer.py
+++ dhtWebServer/appDhtWebServer.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
 #  appDhtWebServer.py
@@ -10,36 +10,59 @@
 	RPi Web Server for DHT captured data  
 '''
 
+import pymysql.cursors
+
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
-import sqlite3
-
 # Retrieve data from database
 def getData():
-	conn=sqlite3.connect('../sensorsData.db')
-	curs=conn.cursor()
+    try:
+        conn = pymysql.connect(
+            user='alex',
+            passwd='!Passw0rd',
+            host='192.168.11.48',
+            db='temp_db'
+        )
+        curs=conn.cursor()
 
-	for row in curs.execute("SELECT * FROM DHT_data ORDER BY timestamp DESC LIMIT 1"):
-		time = str(row[0])
-		temp = row[1]
-		hum = row[2]
-	conn.close()
-	return time, temp, hum
+        curs.execute("SELECT * FROM dht11_sensor ORDER BY probe_date DESC LIMIT 1;")
+        rows = curs.fetchall()
+        print("Total number of rows in Laptop is: ", curs.rowcount)
+
+        time = '2020-1-2'
+        temp = '12'
+        hum = '34'
+        for row in rows:
+            print('No:', row[0], 'Content:', row[1])
+            time = str(row[1])
+            temp = row[2]
+            hum = row[3]
+            print(time, temp, hum)
+        conn.close()
+        return time, temp, hum
+    except pymysql.InternalError as e:
+        print("Error reading data from MySQL table", e)
+    finally:
+        if (conn.open):
+            curs.close()
+            conn.close()
+            print("MySQL connection is closed")
 
 # main route 
 @app.route("/")
 def index():
 	
-	time, temp, hum = getData()
-	templateData = {
-	  'time'	: time,
-      'temp'	: temp,
-      'hum'		: hum
-	}
-	return render_template('index.html', **templateData)
+    time, temp, hum = getData()
+    print(time, temp, hum)
+    templateData = {
+      'time'	: time,
+      'tempLab'	: temp,
+      'humLab'	: hum
+    }
+    return render_template('index.html', **templateData)
 
 
 if __name__ == "__main__":
-   app.run(host='0.0.0.0', port=80, debug=False)
+    app.run(host='0.0.0.0', port=80, debug=False)
 
